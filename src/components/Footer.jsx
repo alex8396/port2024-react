16dfd820a1bef1d5c539cafc108e9c9b21c6292e
alex8396 <alex8396@naver.com>
2024-08-19 20:03:13 +0900

    완성!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,12 @@ const Footer = () => {
         <footer id="footer" role="contentinfo">
             <div className="footer__inner">
                 <h2 className="footer__text">
-                    <div>webstoryboy</div>
+                    <div>limjaechan</div>
                     <div>@webs</div>
                 </h2>
                 <div className="footer__info">
                     <div className="left">
-                        <div className="title">
-                            <a href="/">sign up</a>
-                        </div>
-                        <p className="desc">회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+                        
                     </div>
                     <div className="right">
                         <h3>social</h3>
@@ -30,7 +27,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer__right">
-                    &copy; 2023 webstoryboy<br />
+                    &copy; 2024 limjaechan<br />
                     이 사이트는 리액트를 이용하여 제작하였습니다.
                 </div>
             </div>
